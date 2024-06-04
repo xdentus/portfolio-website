d0821591daba3db6d0c2ab6b9fb5a87b0a9f2b6b
xdentus <xdentus@outlook.com>
2024-06-04 19:48:37 +0200

    theme controller, navbar, pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" data-theme="halloween">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="light">
+      <body className="min-h-screen flex flex-col">
+        <Navbar />
+        <div className="p-6 flex-1">{children}</div>
+      </body>
     </html>
   )
 }
